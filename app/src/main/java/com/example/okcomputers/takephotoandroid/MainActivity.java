13e14e0e847c7fe0f887526ce1c9e1764288b2f2
Muhammad Mashood SIddiquie <40804440+Mashood97@users.noreply.github.com>
2018-09-06 23:01:55 +0500

    How to take Photo In Android Studio
    How to take Photo In Android Studio using Camera

---
+++ app/src/main/java/com/example/okcomputers/takephotoandroid/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.okcomputers.takephotoandroid;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.provider.ContactsContract;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ImageView imageView;
+    private Button btn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btn = (Button)findViewById(R.id.OpenCamera);
+        imageView = (ImageView)findViewById(R.id.imageView);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(intent,0);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Bitmap bitmap = (Bitmap)data.getExtras().get("data");
+        imageView.setImageBitmap(bitmap);
+    }
+}
